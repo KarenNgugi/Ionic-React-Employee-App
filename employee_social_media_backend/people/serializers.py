4224c09a83bc25773a52ac2b57c14c1a33b1cbae
KarenNgugi <karenwngugi12@gmail.com>
2024-09-24 15:38:33 +0300

    added view set

--- employee_social_media_backend/people/serializers.py
+++ employee_social_media_backend/people/serializers.py
@@ -4,4 +4,4 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ["id", "first_name", "last_name", "photo", "position", "department", "email", "phone", "created_at", "updated_at"]
+        fields = ["id", "first_name", "last_name", "photo", "position", "department", "email", "phone", "created_at", "updated_at"] # check if the last two are necessary
