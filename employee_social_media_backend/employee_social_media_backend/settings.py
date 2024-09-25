57ec16f60ed64265202752d0ffb9b171c677ff16
KarenNgugi <karenwngugi12@gmail.com>
2024-09-25 15:39:43 +0300

    added CORS configuration

--- employee_social_media_backend/employee_social_media_backend/settings.py
+++ employee_social_media_backend/employee_social_media_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'people',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'employee_social_media_backend.urls'
@@ -127,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# allow CORS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8100",
+]
