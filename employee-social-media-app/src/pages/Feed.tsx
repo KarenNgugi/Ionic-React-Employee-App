200244de37db78cf85411113e661522f0b3eb701
KarenNgugi <karenwngugi12@gmail.com>
2024-09-26 16:27:20 +0300

    added dynamic feed section

--- employee-social-media-app/src/pages/Feed.tsx
+++ employee-social-media-app/src/pages/Feed.tsx
@@ -18,13 +18,32 @@ import {
   IonCol,
   IonCardContent,
   IonIcon,
+  IonCardTitle,
+  IonCardSubtitle,
 } from "@ionic/react";
 import React, { useState } from "react";
-import { cameraOutline } from "ionicons/icons";
+import { cameraOutline, chatbubbleOutline, heartOutline, shareSocialOutline, imageOutline } from "ionicons/icons";
 
 const Feed: React.FC = () => {
   const [postContent, setPostContent] = useState("");
   const [uploadedImage, setUploadedImage] = useState(false);
+
+  const [posts, setPosts] = useState([
+    {
+        id: 1,
+        profile_picture: "src/images/OnikaMaraj.png",
+        name: "Onika Maraj",
+        timestamp: "Just now",
+        content: "This is a test post",
+    },
+    {
+        id: 2,
+        profile_picture: "src/images/BeyonceKnowles.png",
+        name: "Beyonce Knowles",
+        timestamp: "1 hour ago",
+        content: "Hello everyone, welcome to our new app!",
+    },
+  ]);
 
   // handler for typing in the text area
   const handleTextChange = (e: any) => {
@@ -52,6 +71,7 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
+        {/* user input card */}
         <IonCard>
           <IonCardContent>
             <IonGrid>
@@ -105,7 +125,42 @@ const Feed: React.FC = () => {
             </IonGrid>
           </IonCardContent>
         </IonCard>
-        <IonList></IonList>
+        
+        {/* dynamic feed section */}
+        <IonList>
+            {posts.map((post) => (
+                <IonItem key={post.id}>
+                    <IonCard>
+                        <IonCardHeader>
+                            <IonGrid>
+                                <IonRow>
+                                    <IonCol size="auto">
+                                        <IonImg src={post.profile_picture}
+                                        alt="Poster profile picture" style={{ width: "40px", height: "40px", borderRadius: "40pc"}}/>
+                                    </IonCol>
+                                    <IonCol>
+                                        <IonCardTitle>{post.name}</IonCardTitle>
+                                        <IonCardSubtitle>{post.timestamp}</IonCardSubtitle>
+                                    </IonCol>
+                                </IonRow>
+                            </IonGrid>
+                        </IonCardHeader>
+                        <IonCardContent>{post.content}</IonCardContent>
+                        <IonCardContent>
+                            <IonButton fill="clear">
+                                <IonIcon icon={heartOutline} slot="start" />React
+                            </IonButton>
+                            <IonButton fill="clear">
+                                <IonIcon icon={chatbubbleOutline} slot="start" />Comment
+                            </IonButton>
+                            <IonButton fill="clear">
+                                <IonIcon icon={shareSocialOutline} slot="start" />Share
+                            </IonButton>
+                        </IonCardContent>
+                    </IonCard>
+                </IonItem>
+            ))}
+        </IonList>
       </IonContent>
       <IonFooter className="ion-text-center">&copy; Karen 2024</IonFooter>
     </IonApp>
